bd4caa6e6e4343935dcda788eb4dbb183ca5415b
yuanjinzhong <991242629@qq.com>
2020-06-17 18:30:25 +0800

    rabbit 端口映射

--- rabbitmq/start.sh
+++ rabbitmq/start.sh
@@ -6,7 +6,7 @@ echo "启动rabbitmq,需要保证本地有任意版本的rabbitmq的镜像"
 docker images |grep rabbitmq |awk '{print $1":"$2}'|xargs echo 镜像名；
 
 
-docker images |grep rabbitmq |awk '{ imageName=$1":"$2; print imageName}' | xargs docker run -d -it -p:5672 -p 15672:15672 --name rabbitLearn --hostname rabbitCluster01
+docker images |grep rabbitmq |awk '{ imageName=$1":"$2; print imageName}' | xargs docker run -d -it -p 5672:5672 -p 15672:15672 --name rabbitLearn --hostname rabbitCluster01
 
 # 打印出启动的容器id
 docker  ps |grep rabbitmq |awk '{containerId=$1;print containerId}' |xargs echo  容器id:
